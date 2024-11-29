3987b36d7ddebb6ebe06b10dece930913760bdd8
SteveLorde <mostafa.maher98@gmail.com>
2024-11-29 17:53:09 +0200

    1- Removed Electron and updates packages 2- Added save canvas to clipboard functionality

--- Toolbar.js
+++ Toolbar.js
@@ -1,5 +1,12 @@
-import {canvasContext,ChangeToEraser, ChangeToPen,ClearCanvas, ChangePenColor, IncreaseStroke, DecreaseStroke} from "./DrawingCanvas.js"
-
+import {
+    canvasContext,
+    ChangeToEraser,
+    ChangeToPen,
+    ClearCanvas,
+    ChangePenColor,
+    IncreaseStroke,
+    DecreaseStroke
+} from "./DrawingCanvas.js"
 
 
 const clearwhiteboardbutton = document.getElementById("ClearWhiteBoardButton")
@@ -17,16 +24,18 @@ const redColorButton = document.getElementById('RedColor')
 const blueColorButton = document.getElementById('BlueColor')
 const greenColorButton = document.getElementById('GreenColor')
 
+const saveToClipboardBtn = document.getElementById("SaveToClipboard")
+
 clearwhiteboardbutton.addEventListener('click', ClearCanvas)
 penButton.addEventListener('click', ChangeToPen)
 eraserButton.addEventListener('click', ChangeToEraser)
 //quitButton.addEventListener('click', () => alert("QUIT TEST"))
 
-colorPickerButton.addEventListener('blur',function () {
+colorPickerButton.addEventListener('blur', function () {
     setTimeout(() => colorSwatch.style.display = 'none', 100)
 })
 
-colorPickerButton.addEventListener('click', function() {
+colorPickerButton.addEventListener('click', function () {
     colorSwatch.style.display = 'block'
 })
 
@@ -39,7 +48,8 @@ redColorButton.addEventListener('click', () => {
     ChangePenColor('red')
     selectedColorViewer.style.backgroundColor = "red"
 })
-blueColorButton.addEventListener('click', () => {ChangePenColor('blue')
+blueColorButton.addEventListener('click', () => {
+    ChangePenColor('blue')
     selectedColorViewer.style.backgroundColor = "blue"
 })
 greenColorButton.addEventListener('click', () => {
@@ -50,3 +60,17 @@ greenColorButton.addEventListener('click', () => {
 
 decreaseStrokeButton.addEventListener('click', () => DecreaseStroke())
 increaseStrokeButton.addEventListener('click', () => IncreaseStroke())
+
+saveToClipboardBtn.addEventListener('click', async () => {
+    try {
+        const blob = await new Promise(resolve => canvasContext.canvas.toBlob(resolve, 'image/png'))
+
+        const clipboardItem = new ClipboardItem({'image/png': blob})
+
+        await navigator.clipboard.write([clipboardItem])
+
+        alert('Canvas image copied to clipboard!')
+    } catch (err) {
+        console.log('Failed to copy image to clipboard:', err)
+    }
+})
